9dfa081aa5f93cbd8fa98af96f5c1cd48e4c959d
GEOMATIC\ahowie <ahowie@geomatic.com.au>
2019-03-21 13:58:59 +1100

    new

--- send_sns.py
+++ send_sns.py
@@ -2,8 +2,8 @@ import boto3
 
 client = boto3.client('sns')    
 
-def send_sns(sns_topic_arn="arn:aws:sns:ap-southeast-2:096841396245:ITOPSOnCall",sns_message="SaltStack"):
+def send_sns(sns_topic_arn=None,sns_message=None):
 
     print(client.publish(TopicArn=sns_topic_arn,Message=sns_message))
 
-send_sns(sns_message="new message")
+send_sns(sns_topic_arn="arn:aws:sns:ap-southeast-2:096841396245:ITOPSOnCall",sns_message="new message")

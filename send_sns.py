c947c15531cf09c9c346ad95f254d112a6a464ce
GEOMATIC\ahowie <ahowie@geomatic.com.au>
2019-03-21 14:00:27 +1100

    new

--- send_sns.py
+++ send_sns.py
@@ -5,5 +5,3 @@ client = boto3.client('sns')
 def send_sns(sns_topic_arn=None,sns_message=None):
 
     print(client.publish(TopicArn=sns_topic_arn,Message=sns_message))
-
-send_sns(sns_topic_arn="arn:aws:sns:ap-southeast-2:096841396245:ITOPSOnCall",sns_message="new message")
